f04f37d3eebd5982982c5b1cc1831b36f1dd693a
Dmitriy Kiyatkin <d.kiyatkin@itlf.ru>
2012-07-03 14:33:32 +0400

    0.0.7

--- index.js
+++ index.js
@@ -7,33 +7,34 @@ module.exports = function(options) {
 	var _infra = options.Infra.init();
 	return function(req, res, next) {
 		// собрать слои, если не собраны и проверить есть ли такой state
-		var state = '/';
 		try {
-			state = decodeURI(req.originalUrl);
+			var state = decodeURI(req.originalUrl);
+			if (!url.parse(state).search) { // если есть ? значит это не к infrajs
+				_infra.checkExists(state, function(exist) {
+					if (exist) {
+						var infra = options.Infra.init();
+						infra.log.logger = options.logger;
+						getInfraHtml(infra, options.index_html, state, req, options.root_dir, function(status_code, html) {
+							res.writeHead(status_code, { 'Content-Type': 'text/html' });
+							res.end(html);
+						});
+					// проверка на последний слэш
+					} else if (state.slice(-1) != '/') {
+						state = state + '/';
+						_infra.checkExists(state, function(exist) {
+							if (exist) {
+								res.writeHead(301, {"Location":req.originalUrl+'/'}); // moved permanently
+								res.end();
+							} else { next(); }
+						});
+					} else { next(); }
+				});
+			} else { next(); }
 		} catch(e) {
 			console.log(e);
+			res.writeHead(301, {"Location":'/'}); // moved permanently
+			res.end();
 		}
-		if (!url.parse(state).search) { // если есть ? значит это не к infrajs
-			_infra.checkExists(state, function(exist) {
-				if (exist) {
-					var infra = options.Infra.init();
-					infra.log.logger = options.logger;
-					getInfraHtml(infra, options.index_html, state, req, options.root_dir, function(status_code, html) {
-						res.writeHead(status_code, { 'Content-Type': 'text/html' });
-						res.end(html);
-					});
-				// проверка на последний слэш
-				} else if (state.slice(-1) != '/') {
-					state = state + '/';
-					_infra.checkExists(state, function(exist) {
-						if (exist) {
-							res.writeHead(301, {"Location":req.originalUrl+'/'}); // moved permanently
-							res.end();
-						} else { next(); }
-					});
-				} else { next(); }
-			});
-		} else { next(); }
 	};
 };
 module.exports.regExpStr_njs = getInfraHtml.regExpStr_njs;
